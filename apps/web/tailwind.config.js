0291750f059486d85d2ea496e3f62e98d156803c
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-05-27 17:45:46 +0600

    feat: :hammer: Setup tailwindcss and UI library

---
+++ apps/web/tailwind.config.js
@@ -0,0 +1,39 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1200px',
+    },
+    container: { center: true, padding: '1rem' },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        roboto: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        primary: '#130f40',
+        // primary: '#FD3D57',
+      },
+    },
+  },
+  variants: {
+    extend: {
+      display: ['group-hover'],
+      visibility: ['group-hover'],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
