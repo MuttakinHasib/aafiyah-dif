0475d57b931bf218fde24a9629a77cb2336b625f
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-07-31 07:42:40 +0000

    feat: :hammer: update product entity

--- apps/api/src/app/modules/products/entities/product.entity.ts
+++ apps/api/src/app/modules/products/entities/product.entity.ts
@@ -1,6 +1,14 @@
 import { CoreEntity } from '@aafiyah/common';
-import { ObjectType, Field, Int,  } from '@nestjs/graphql';
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  IsArray,
+  IsBoolean,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  IsUrl,
+} from 'class-validator';
 import { Column } from 'typeorm';
 
 @ObjectType()
@@ -9,24 +17,51 @@ export class Product extends CoreEntity {
   @Column()
   @IsNotEmpty()
   @IsString()
-  name: string
+  name: string;
 
   @Field()
   @Column()
   @IsNotEmpty()
   @IsString()
-  description: string
+  description: string;
 
   @Field(() => Int)
   @Column()
   @IsNotEmpty()
   @IsNumber()
-  price: number
+  price: number;
+
+  @Field(() => Int, { nullable: true })
+  @Column({ nullable: true })
+  @IsOptional()
+  @IsNumber()
+  discount?: number;
 
   @Field(() => Int)
-  @Column({default: 0})
+  @Column({ default: 0 })
   @IsNotEmpty()
   @IsNumber()
-  countInStock: number
+  countInStock: number;
 
+  @Field()
+  @Column({ type: 'simple-array' })
+  @IsArray()
+  @IsUrl({}, { each: true })
+  images: string[];
+
+  @Field()
+  @Column({ unique: true })
+  @IsNotEmpty()
+  @IsString()
+  sku: string;
+
+  @Field()
+  @Column({ type: 'simple-array' })
+  @IsArray()
+  tags: string[];
+
+  @Field(() => Boolean)
+  @Column({ type: 'boolean', default: false })
+  @IsBoolean()
+  featured: boolean;
 }
