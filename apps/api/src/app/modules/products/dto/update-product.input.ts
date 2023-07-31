fb76a85fe3b4ea093f0b82e5141c6a0d177d3231
Muttakin Islam Hasib <muttakinislamhasib@gmail.com>
2023-07-31 09:04:09 +0000

    feat: :hammer: add update product dto

--- apps/api/src/app/modules/products/dto/update-product.input.ts
+++ apps/api/src/app/modules/products/dto/update-product.input.ts
@@ -1,8 +1,5 @@
-import { CreateProductInput } from './create-product.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
+import { Product } from '../entities/product.entity';
+import { InputType, PartialType } from '@nestjs/graphql';
 
 @InputType()
-export class UpdateProductInput extends PartialType(CreateProductInput) {
-  @Field(() => Int)
-  id: number;
-}
+export class UpdateProductInput extends PartialType(Product, InputType) {}
